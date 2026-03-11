46e89bf1379653eb68d789b53cc0a2737dfaf519
363434879 <363434879@gotvdsb.ca>
2026-03-11 12:12:08 -0400

    Add project files.

---
+++ 7.1- While Loops Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _7._1__While_Loops_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+         
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
